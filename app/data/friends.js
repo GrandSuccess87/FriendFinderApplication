4cd401b68aa05e19602a4131ac1979b6386f1c61
Quintessa <anderson.quintessa@gmail.com>
2019-08-29 16:54:46 -0500

    Modified css background colors and updated image links

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,27 +1,27 @@
 var friendArray =
-[ 
+[
     {
     name: "Quintessa",
-    photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAv2AAAAJGEzNjllOTQyLTE4YWMtNDliMC1iYzdmLWUwNGJiMWEzZDM1Mg.jpg",
+    photo: "https://media.licdn.com/dms/image/C5603AQEoKPRWNgG0Mg/profile-displayphoto-shrink_200_200/0?e=1572480000&v=beta&t=VynztOfyCK5MPeXgciX4ey5W-SBdwJVwr7pir3KBwKE",
     scores:[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
     },
     {
     name: "Jake Gyllenhaal",
-    photo:"https://i.ytimg.com/vi/Vtt32wBwXFw/maxresdefault.jpg",
+    photo:"https://peopledotcom.files.wordpress.com/2017/05/jake-gyllenhaal-2000.jpg",
     scores:[5, 1, 3, 4, 5, 5, 2, 5, 3, 4]
-    
+
     },
     {
       name: "Shia LaBeouf",
       photo:"https://www.famousbirthdays.com/headshots/shia-laBeouf-4.jpg",
       scores:[2, 1, 4, 4, 5, 1, 2, 5, 4, 1]
     },
-    { 
+    {
     name: "Michelle Obama",
     photo:"http://thereelnetwork.net/wp-content/uploads/2016/04/michelle-obama.jpg",
     scores:[5, 1, 4, 1, 5, 1, 2, 2, 4, 3]
 
-    }, 
+    },
     {
       name: "Barack Obama",
       photo:"http://images.fandango.com/ImageRenderer/300/0/redesign/static/img/default_poster.png/0/images/masterrepository/performer%20images/p504337/barackobama-1.jpg",
@@ -31,12 +31,12 @@ var friendArray =
       name: "Ellen Degeneres",
       photo:"https://media.architecturaldigest.com/photos/55f9e0d100a69f791feb4161/4:3/w_384/dam-images-daily-2015-06-ed-by-ellen-ed-ellen-degeneres-1.jpg",
       scores:[3, 1, 1, 4, 3, 4, 2, 5, 4, 1]
-    }, 
+    },
     {
       name: "Michael Ealy",
       photo:"http://www.ebony.com/wp-content/uploads/2016/11/Ealy-Caro-603x377.jpg",
       scores:[1, 1, 4, 5, 2, 5, 2, 5, 3, 4]
-    }, 
+    },
     {
       name: "Olivia Pope",
       photo:"https://precisemuzic.files.wordpress.com/2013/07/olivia-pope-1.png",
@@ -49,5 +49,5 @@ var friendArray =
     }
 ];
 
-  
+
   module.exports = friendArray;
